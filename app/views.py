dcef12bbf53e74d9b556b2220336ca07f215ba1b
Eduard <eduard@eduard-VirtualBox.(none)>
2013-03-13 12:50:38 +0100

    multisobres

--- app/views.py
+++ app/views.py
@@ -1,5 +1,26 @@
+from flask import render_template
 from app import app
 @app.route('/')
 @app.route('/home')
 def home():
-return "Nothing here yet!!!"
+	user = { 'name': 'Barcenas','city':'Madrid' } # Mockup User
+
+	sobres = [
+		{
+			'Company':{ 'name': 'ACS'},
+			'Amount' : '100.000EUR'
+		},
+		{
+			'Company':{ 'name': 'BANKIA'},
+			'Amount' : '199999.9999.000EUR'
+		},
+		{
+			'Company':{ 'name': 'TELEFONICA'},
+			'Amount' : '100.000.000.000EUR'
+		},
+	]
+
+
+	return render_template("index.html",
+		pagetitle = 'Home',
+		user = user, envelopes=sobres)
